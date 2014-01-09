f538381c744d380a966cb40ad2d86ef4bc3d23cf
RepeatPan <zaubermuffin+repeatpan@gmail.com>
2014-01-09 23:18:32 +0100

    Push the version already; this is going to be a long night.

--- api.lua
+++ api.lua
@@ -48,7 +48,7 @@ SOFTWARE.
 require('base')
 
 -- Our ridiculously inflated version
-local VERSION = 1402
+local VERSION = 2199
 
 -- If RP has already been initialized, abort
 -- In related news, >:[ this strict thing.
