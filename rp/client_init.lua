bc535508a1a81003a22f89b4491198728655bc0f
RepeatPan <zaubermuffin+repeatpan@gmail.com>
2014-01-24 14:09:55 +0100

    Added (temporary) rp.keyboard_input_disabled.
    Setting this value to "true" will block any "is_key_down" event and prevent capture_input:on_input events from running if they are keyboard input events.

--- rp/client_init.lua
+++ rp/client_init.lua
@@ -3,5 +3,43 @@ local rp = require('api')
 local LM = require('load_mods')
 local lm = LM()
 rp.CONFIG = nil
+
+-- HAAACKS
+local client = _radiant.client
+
+local old_capture_input = client.capture_input
+rp.keyboard_input_disabled = false
+
+local function patch_on_input(input_capture)
+	local on_input = input_capture.on_input
+	
+	function input_capture:on_input(callback)
+		return on_input(self, 
+			function(event, ...)
+				if event.type == client.Input.KEYBOARD and rp.keyboard_input_disabled then
+					return true
+				end
+				return callback(event, ...)
+			end
+			)
+	end
+end
+
+function client.capture_input(...)
+	local ret = old_capture_input(...)
+	
+	patch_on_input(ret)
+	return ret
+end
+
+local old_is_key_down = client.is_key_down
+function client.is_key_down(key)
+	if rp.keyboard_input_disabled then
+		return false
+	end
+	
+	return old_is_key_down(key)
+end
+
 -- Load the mods
 lm:load_mods()
